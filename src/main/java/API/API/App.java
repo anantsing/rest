7e9d22031978fd226f3a4386dad190c70a080d21
SCOREME-LT-ANANT <anantbhadoria99@gmail.com>
2023-07-26 23:01:45 +0530

    qa commit

--- src/main/java/API/API/App.java
+++ src/main/java/API/API/App.java
@@ -4,10 +4,14 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("skjdbcj");
-   
+    App ap =new App();
+    ap.m1();
     
   }
   public static void  m1() {
 	  System.out.println("kjsndckjn");
+  }
+  public static void  m11() {
+	  System.out.println("@@@@@@@@@kdckjn");
   }
 }
