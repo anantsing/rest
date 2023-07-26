325f0f5d3d814c6e625e6f76edabdeffe4b3cf26
SCOREME-LT-ANANT <anantbhadoria99@gmail.com>
2023-07-26 22:52:40 +0530

    sjn

--- src/main/java/API/API/App.java
+++ src/main/java/API/API/App.java
@@ -4,6 +4,10 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("skjdbcj");
+   
     
+  }
+  public static void  m1() {
+	  System.out.println("kjsndckjn");
   }
 }
